a4cd164ebf67915fbd078de9a0fa419c46d1209c
chof747 <chof@gmx.at>
2021-08-19 22:29:51 +0200

    Added environment specific behaviour to config_sample.h
    - removed message endpoint from platformio.ini
    - added environment specifics for mqtt and api endpoint

--- include/config_sample.h
+++ include/config_sample.h
@@ -5,11 +5,25 @@
 #define WIFI_SSID       ""
 #define WIFI_PASSWORD   ""
 
-#define MQTT_BROKER     "127.0.0.1"
-#define MQTT_USER       ""
-#define MQTT_PASSWORD   ""
-
+#if ENVIRONMENT == 1 //production
+  #define MQTT_BROKER     "10.0.1.42"
+  #define MQTT_USER       ""
+  #define MQTT_PASSWORD   ""
+#elif ENVIRONMENT == 0 //development
+  #define MQTT_BROKER     "127.0.0.1"
+  #define MQTT_USER       ""
+  #define MQTT_PASSWORD   ""
+#else
+#endif
 #define MQTT_SUBSCRIPTIONS 2
+
+
+#if ENVIRONMENT == 1 //production
+  #define MESSAGE_API_ENDPOINT ""
+#elif ENVIRONMENT == 0 //development
+  #define MESSAGE_API_ENDPOINT "http://127.0.0.1:3000/api/status/"
+#else
+#endif
 
 #ifndef CONTROLLER_TIMEOUT 
     #define CONTROLLER_TIMEOUT 30000
